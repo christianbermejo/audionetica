ba13035e463826652f2487395017afdbbc3d3bd4
Christian-Bermejo <Christian_Bermejo@trendmicro.com>
2025-06-24 22:41:54 +0800

    Adjust min silence sensitivity
    Adjust min silence sensitivity for shorter sentences

--- stream_webrtc_s2s.py
+++ stream_webrtc_s2s.py
@@ -152,7 +152,7 @@ def app_sst(
     enter_silence_threshold_factor = 1.0 / sensitivity  # Lower to enter silence state
     exit_silence_threshold_factor = 1.2 * sensitivity   # Higher to exit silence state
 
-    min_silence_duration = 0.5 / sensitivity  # seconds, adjusted by sensitivity
+    min_silence_duration = 0.2 / sensitivity  # seconds, adjusted by sensitivity (shortened for shorter sentences)
     max_silence_duration = 2.0 * sensitivity  # seconds, adjusted by sensitivity
 
     while True:
